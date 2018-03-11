83ea18054a3e401bca988811a353fa24f54a33fc
goyalsushant <goyalsushant2@gmail.com>
2018-03-11 21:50:45 +0530

    updated

--- app.js
+++ app.js
@@ -3,39 +3,20 @@
 var App = angular.module('routingDemoApp',['ui.router']);
 
 App.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
-				// For any unmatched url, send to /business
-				$urlRouterProvider.otherwise("/business")
-				
-				$stateProvider
-						.state('business', {
-							url: "/business",
-							templateUrl: "business.html"
-						})
-						.state('business.products', {
-							url: "/products",
-							templateUrl: "products.html",
-							controller: function($scope){
-								$scope.products = ["Computer", "Printers", "Phones", "Bags"];
-							}
-						})
-						.state('business.services', {
-							url: "/services",
-							templateUrl: "services.html",
-							controller: function($scope){
-								$scope.services = ["Selling", "Support", "Delivery", "Reparation"];
-							}
-						})
-
-						.state('portfolio', {
-							url: "/portfolio",
-							views: {
-								"" 	:    { templateUrl: "portfolio.html" },
-								"view1@portfolio": { template: "Write whatever you want, it's your virtual company." },
-								"view2@portfolio": { templateUrl: "clients.html" ,
-									controller: function($scope){
-											$scope.clients = ["HP", "IBM", "MicroSoft"];
-									}
-								}
-							}
-						})
-			}]);
+	$urlRouterProvider
+      .otherwise('/');
+	$stateProvider
+		.state('profile', {
+			url: "/profile",
+			templateUrl: "profile.html"
+		})
+		.state('repositories', {
+			url: "/repositories",
+			views: {
+				"" 	:    { templateUrl: "repositories.html" },
+			}
+		})
+	}]);
+App.config(["$locationProvider", function($locationProvider) {
+	$locationProvider.html5Mode(true);
+}]);
